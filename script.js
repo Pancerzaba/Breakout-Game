ccb87454648c05265e763674333a96f44d903cc3
Pancerzaba <marta.rozmyslowicz21@gmail.com>
2020-07-16 20:49:35 +0200

    Move Paddle

--- script.js
+++ script.js
@@ -87,15 +87,60 @@ function drawBricks(){
     })
 }
 
+//Move paddle on canvas
+function movePaddle(){
+    paddle.x += paddle.dx;
+
+    //Wall detaction
+    if(paddle.x + paddle.w >canvas.width){
+        paddle.x = canvas.width - paddle.w;
+    }
+
+    if(paddle.x < 0){
+        paddle.x = 0;
+    }
+}
+
 //Draw everything
 function draw(){
+    //Clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     drawball();
     drawPaddle();
     drawScore();
     drawBricks();
 }
 
-draw();
+//Update canvas drawing and animation
+function update(){
+    movePaddle();
+
+    draw();
+
+    requestAnimationFrame(update);
+}
+
+update();
+
+//Keydown event
+function keyDown(e){
+    if(e.key === 'Right' || e.key === 'ArrowRight'){
+        paddle.dx = paddle.speed;
+    }else if(e.key === 'Left' || e.key === 'ArrowLeft'){
+        paddle.dx = -paddle.speed;
+    }
+}
+
+function keyUp(e){
+    if(e.key === 'Right' || e.key === 'ArrowRight' || e.key === 'Left' || e.key === 'ArrowLeft'){
+        paddle.dx = 0;
+    }
+}
+
+//Keyboard event handlers
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
 
 //Rules and close event handlers
 rulesBtn.addEventListener('click', ()=> rules.classList.add('show'));
